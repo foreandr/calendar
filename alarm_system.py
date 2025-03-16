a413a5e76564f4585b49271053842d3276a4401e
foreandr <foreandr@gmail.com>
2025-03-16 10:42:02 -0400

    weird issue

--- alarm_system.py
+++ alarm_system.py
@@ -1,3 +1,13 @@
 # notify me every 3 hours or sorted
 
 # give me option to snooze for N hours, modify this func, wont work,idk have to modify the task scheudler
+
+e1 = (3 * 1) + (7 *1) - (2 * -1) - (5 * 2) 
+e2 = (8) + (14) - (2 * -1) - (11*2) 
+e3 = (-4) + (-8) + (-2*-1) + (6*2) 
+e4 = (12) + (22) + (-4*-1) - (17*2) 
+
+print("e1:",e1)
+print("e2:",e2)
+print("e3:",e3)
+print("e4",e4)
